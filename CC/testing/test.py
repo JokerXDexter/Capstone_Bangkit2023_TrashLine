7890c5b6dc33c4326068940ab90383a3b5a13be4
JokerXDexter <rahmatyahya59@gmail.com>
2023-06-16 20:43:30 +0700

    add-file4

--- CC/testing/test.py
+++ CC/testing/test.py
@@ -1,6 +1,6 @@
 import requests
 
-resp = requests.post("https://realtrashline-w5bu4l3kga-et.a.run.app/predict",
-                     files={'file': open('test.jpg', 'rb')})
+resp = requests.post("https://trashlinefixv2-w5bu4l3kga-et.a.run.app/predict",
+                     files={'file': open('test3.jpg', 'rb')})
 
 print(resp.json())
